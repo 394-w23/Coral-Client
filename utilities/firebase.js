479583d0440f11cb208d4122f0ff0823432701f0
alfenn <52635707+alfenn@users.noreply.github.com>
2023-02-11 11:02:59 -0600

    configure firebase

---
+++ utilities/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDgVg3iVvgnV3iAWlKvfdvDsUEmjcfUtMs",
+  authDomain: "coralclient-59585.firebaseapp.com",
+  databaseURL: "https://coralclient-59585-default-rtdb.firebaseio.com",
+  projectId: "coralclient-59585",
+  storageBucket: "coralclient-59585.appspot.com",
+  messagingSenderId: "708701666248",
+  appId: "1:708701666248:web:9fae4b5776c967259c1efd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
