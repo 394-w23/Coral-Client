f62cf8a79bb4e36871f3aa042c3c438c89be4117
Nicholas Tsung <nicktsung@gmail.com>
2023-02-14 20:07:13 -0600

    added accordian component, needs bug fixing with expanding and collapsing subtasks

--- src/components/Subtask.jsx
+++ src/components/Subtask.jsx
@@ -5,6 +5,7 @@ import {
   CardFooter,
   Typography,
 } from "@material-tailwind/react";
+import 'tw-elements';
 
 const Subtask = ({ subtaskData }) => {
   // const [count, setCount] = useState(0);
@@ -16,15 +17,37 @@ const Subtask = ({ subtaskData }) => {
   return (
     <div className="flex justify-center">
       <Card className="w-[70vw] text-centered my-4 border-solid border-2">
-        <CardBody>
-          <Typography variant="h5" className="mb-2">
-            {subtaskTitle}
-          </Typography>
 
-          <Typography variant="paragraph" color="blue-gray" className="mb-2">
-            {subtaskInfo}
-          </Typography>
-        </CardBody>
+          <div className="accordion" id="accordionExample">
+            <div className="accordion-item bg-white border border-gray-200">
+              <h2 className="accordion-header mb-0" id="headingOne">
+                <button className="
+        accordion-button
+        relative
+        flex
+        items-center
+        w-full
+        py-4
+        px-5
+        text-base text-gray-800 text-left
+        bg-white
+        border-0
+        rounded-none
+        transition
+        focus:outline-none
+      " type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true"
+                  aria-controls="collapseOne">
+                  {subtaskTitle}
+                </button>
+              </h2>
+              <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne"
+                data-bs-parent="#accordionExample">
+                <div className="accordion-body py-4 px-5">
+                  {subtaskInfo}
+                </div>
+              </div>
+            </div>
+            </div>
       </Card>
     </div>
   );
