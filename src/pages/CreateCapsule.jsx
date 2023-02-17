e93a62c775b89fe0b2cc7f105b29dada8385ac09
alfenn <52635707+alfenn@users.noreply.github.com>
2023-02-16 19:40:36 -0600

    tried implementing dropdown, implemnted icons

--- src/pages/CreateCapsule.jsx
+++ src/pages/CreateCapsule.jsx
@@ -9,13 +9,17 @@ import {
 } from "@material-tailwind/react";
 
 import Navigation from "../components/Navigation/Navigation";
+import AddInfo from "../components/AddInfo/AddInfo";
 
 const CreateCapsule = ({ eventID, eventData }) => {
  
   return (
     <div>
       <p>hello</p>
-      
+      <AddInfo title={"memories"}/>
+      <AddInfo title={"tasks"}/>
+      <AddInfo title={"goodwill"}/>
+
     </div>
   );
 };
