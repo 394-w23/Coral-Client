c77311561ee5bb6efc8fad1aca5e0854ed69f44a
Nicholas Tsung <nicktsung@gmail.com>
2023-02-17 20:36:40 -0600

    Implement new capsule screen

--- src/pages/NewCapsule.jsx
+++ src/pages/NewCapsule.jsx
@@ -7,6 +7,8 @@ import {
   CardBody,
   CardFooter,
 } from "@material-tailwind/react";
+import "./NewCapsule/NewCapsule.css";
+import logo from "../../public/new_capsule.png";
 
 import Navigation from "../components/Navigation/Navigation";
 import { GoKebabVertical } from "react-icons/go";
@@ -24,6 +26,24 @@ const NewCapsule = ({ name }) => {
                 </a>
             </div>
             </nav>
+            <div class="flex justify-center">
+                <img src={logo} alt="Logo" style={{width: "600px", height: "auto"}}></img>
+            </div>
+            <div class="content-center capsule-description" >
+                <h1 class="user">
+                    <b>Emma,</b>
+                </h1>
+                <p class="description">
+                    Your capsule of love has been created
+                </p>
+
+            </div>
+            <div class="flex justify-center">
+                <div class="container form-check form-switch">
+                    <input class="button-center justify-center form-check-input appearance-none w-36  rounded-full float-left h-5 align-top bg-no-repeat bg-contain bg-gray-300 focus:outline-none cursor-pointer shadow-sm" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+                    <label class="text-center form-check-label inline-block text-gray-800" for="flexSwitchCheckDefault">activate capsule</label>
+                </div>
+            </div>
         </div>
     )
 };
