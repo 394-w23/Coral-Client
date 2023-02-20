000925e167745aa0f57af5b66d31ddffef6bc23e
Chris Naing <clnaing23@gmail.com>
2023-02-19 20:48:20 -0600

    coursel buggy

--- src/pages/CapsulePreview.jsx
+++ src/pages/CapsulePreview.jsx
@@ -1,71 +1,82 @@
 import { useState } from "react";
 import pic from "../../public/Screenshot 2023-02-19 195026.jpg";
 import PreviewCard from "../components/PreviewCard";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { Button } from "@material-tailwind/react";
+import { Carousel } from "flowbite-react";
 import {
-    Card,
-    CardHeader,
-    CardBody,
-    CardFooter,
-    Typography,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
 } from "@material-tailwind/react";
 
-const CapsulePreview = ({ }) => {
-    return (
-        <div>
-            <Typography
-                variant="h1"
-                color="blue-gray"
-                className="mb-2 secondary-font-large header-text"
-            >
-                Your Capsule Summary
-            </Typography>
-            <Typography
-                variant="h2"
-                color="blue-gray"
-                className="mb-2 secondary-font-large header-text"
-            >
-                Memories
-            </Typography>
-            <div>
-                <img src={pic}></img>
-            </div>
-            <Typography
-                variant="h2"
-                color="blue-gray"
-                className="mb-2 secondary-font-large header-text"
-            >
-                Notes
-            </Typography>
-            <div className="flex flex-col items-center">
-                <PreviewCard
-                    title="To Mr. John Sanchez"
-                    description="ilysm"></PreviewCard>
-                <PreviewCard
-                    title="To Maya"
-                    description="Please please please cancel my Hulu sub or ur disowned">
-
-                </PreviewCard>
-            </div>
-            <Typography
-                variant="h2"
-                color="blue-gray"
-                className="mb-2 secondary-font-large header-text"
-            >
-                Goodwill
-            </Typography>
-            <div className="flex flex-col items-center">
-                <PreviewCard
-                    title="Red Cross"
-                    description="Donate all my money there please">
-                </PreviewCard>
-            </div>
-            <Link to="/newCapsule">
-                <Button className="primary-blue-background next-button">Next</Button>
-            </Link>
+const CapsulePreview = ({}) => {
+  return (
+    <div>
+      <Typography
+        variant="h1"
+        color="blue-gray"
+        className="mb-2 secondary-font-large header-text"
+      >
+        Your Capsule Summary
+      </Typography>
+      <Typography
+        variant="h2"
+        color="blue-gray"
+        className="mb-2 secondary-font-large header-text"
+      >
+        Memories
+      </Typography>
+      <div>
+        <img src={pic}></img>
+      </div>
+        <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+          <Carousel>
+            <img src={pic} alt="..." />
+            <img src={pic} alt="..." />
+            <img src={pic} alt="..." />
+            <img src={pic} alt="..." />
+            <img src={pic} alt="..." />
+          </Carousel>
         </div>
-    )
-}
+
+      <Typography
+        variant="h2"
+        color="blue-gray"
+        className="mb-2 secondary-font-large header-text"
+      >
+        Notes
+      </Typography>
+      <div className="flex flex-col items-center">
+        <PreviewCard
+          title="To Mr. John Sanchez"
+          description="ilysm"
+        ></PreviewCard>
+        <PreviewCard
+          title="To Maya"
+          description="Please please please cancel my Hulu sub or ur disowned"
+        ></PreviewCard>
+      </div>
+      <Typography
+        variant="h2"
+        color="blue-gray"
+        className="mb-2 secondary-font-large header-text"
+      >
+        Goodwill
+      </Typography>
+      <div className="flex flex-col items-center">
+        <PreviewCard
+          title="Red Cross"
+          description="Donate all my money there please"
+        ></PreviewCard>
+      </div>
+      <Link to="/newCapsule">
+        <Button className="primary-blue-background next-button">Next</Button>
+      </Link>
+    </div>
+  );
+};
 
 export default CapsulePreview;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 const withMT = require("@material-tailwind/react/utils/withMT");
 
 module.exports = withMT({
-  content: ["./src/**/*.{js,jsx,ts,tsx}", './src/**/*.{html,js}', './node_modules/tw-elements/dist/js/**/*.js'],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", './src/**/*.{html,js}', './node_modules/tw-elements/dist/js/**/*.js', "./node_modules/flowbite/**/*.js", "node_modules/flowbite-react/*/.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
     colors: {
       "greenPrimary": "#a5be00"
     }
   },
-  plugins: [require('tw-elements/dist/plugin')],
+  plugins: [require('tw-elements/dist/plugin'), require('flowbite/plugin')],
 });
