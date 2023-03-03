5d1d0253f287d1d2d0741a36aaee2190dc7dcc36
Nicholas Tsung <nicktsung@gmail.com>
2023-03-02 19:48:12 -0600

    Switch textinput to normal inputs for add note modal

--- src/modals/AddNoteModal.jsx
+++ src/modals/AddNoteModal.jsx
@@ -7,9 +7,10 @@ const AddNoteModal = ({ showModal, onCloseModal, userData, capsuleData }) => {
   const [update] = useDbUpdate(`/capsules/`);
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
 
-  const handleSubmit = () => {
-    const title = document.getElementById("add-note-title").value;
-    const msg = document.getElementById("add-note-msg").value;
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const title = event.target.title.value;
+    const msg = event.target.msg.value;
     if (title.length !== 0 && msg.length !== 0) {
       let updatedNotes = capsuleData["notes"];
       updatedNotes.push({content:msg, title:title});
@@ -29,8 +30,38 @@ const AddNoteModal = ({ showModal, onCloseModal, userData, capsuleData }) => {
     <Modal show={showModal} size="md" popup={true} onClose={onCloseModal}>
       <Modal.Header />
       <Modal.Body>
-        <div className="space-y-6 px-6 pb-4 sm:pb-6 lg:px-8 xl:pb-8">
+        <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
           <h3 className="text-xl font-medium text-gray-900 dark:text-white">
+            Add Task
+          </h3>
+          {showSuccessMessage && (
+            <div className="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
+              <span className="font-medium">Successfully added!</span>
+            </div>
+          )}
+          <div>
+              <label htmlFor="title" value="Title">
+                Title:
+              <input name="title" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"   />
+
+              </label>
+          </div>
+          <div>
+              
+              <label htmlFor="message" value="Your message">
+              Your message: 
+              <input name="msg" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+              </label>
+
+          </div>
+          <div>
+            <Button onClick={handleSubmit} className="text-white bg-indigo-600 relative cursor-default select-none py-2 pl-3 pr-12 dropdown-option secondary-green-background">
+              Upload
+            </Button>
+          </div>
+        </form>
+        <div className="space-y-6 px-6 pb-4 sm:pb-6 lg:px-8 xl:pb-8">
+          {/* <h3 className="text-xl font-medium text-gray-900 dark:text-white">
             Add Task
           </h3>
           {showSuccessMessage && (
@@ -54,7 +85,7 @@ const AddNoteModal = ({ showModal, onCloseModal, userData, capsuleData }) => {
             <Button onClick={handleSubmit} className="text-white bg-indigo-600 relative cursor-default select-none py-2 pl-3 pr-12 dropdown-option secondary-green-background">
               Upload
             </Button>
-          </div>
+          </div> */}
         </div>
       </Modal.Body>
     </Modal>
